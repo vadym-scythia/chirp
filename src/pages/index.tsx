1937e29fd0bdc1dc01959f8b4ce50c62b83bf577
Vadym-Mandrichenko_bdgh <vadymmandrichenko@gmail.com>
2023-04-28 19:16:07 +0300

    Add error handling with toasts, posting loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,14 @@
 import { SignInButton, useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
-import { LoadingPage } from "~/components/loading"
+import { LoadingPage, LoadingSpinner } from "~/components/loading"
 import { api } from "~/utils/api";
 import type { RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime"
 import Image from "next/image";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 dayjs.extend(relativeTime);
 
@@ -23,6 +24,14 @@ const CreatePostWizard = () => {
       setInput("");
       void ctx.posts.getAll.invalidate();
     },
+    onError: (e) => {
+      const errorMessage = e.data?.zodError?.fieldErrors.content;
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
+      } else {
+        toast.error("Failed to post! Try again later!")
+      }
+    },
   });
 
   if (!user) return null;
@@ -36,8 +45,18 @@ const CreatePostWizard = () => {
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            e.preventDefault();
+            if (input !== "") {
+              mutate({ content: input });
+            }
+          }
+        }}
         disabled={isPosting} />
-      <button onClick={() => mutate({ content: input })}>Post</button>
+      {input !== "" && !isPosting && (
+        <button onClick={() => mutate({ content: input })}>Post</button>)}
+      {isPosting && <div className="flex justify-center items-center"><LoadingSpinner size={20} /></div>}
     </div>);
 }
 
@@ -87,7 +106,6 @@ const Home: NextPage = () => {
             {!isSignedIn && <div className="flex justify-center"><SignInButton /></div>}
             {isSignedIn && <CreatePostWizard />}
           </div>
-
           <Feed />
         </div>
       </main>
